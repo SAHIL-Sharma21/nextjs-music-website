e168d324114e1d1f6e600b8ac535108654f48f53
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-05-22 12:15:55 +0530

    deleted basic file

--- src/app/helloNextjs/Heros/page.tsx
+++
@@ -1,9 +0,0 @@
-//making nested route in helloNextjs
-
-export default function HeroPage(){
-    return(
-        <>
-            <h2>This is hero page inside helloNext js.</h2>
-        </>
-    );
-}
